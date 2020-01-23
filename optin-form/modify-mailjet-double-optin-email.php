bfbc071381747fb409c1f006ea40281dcfe903d2
Collins Agbonghama <Collizo4sky@gmail.com>
2020-01-23 10:07:44 +0100

    Create modify-mailjet-double-optin-email.php

---
+++ optin-form/modify-mailjet-double-optin-email.php
@@ -0,0 +1,13 @@
+<?php
+
+add_filter('mo_mailjet_confirm_email_subject', function () {
+    return 'Confirm your subscription';
+});
+
+add_filter('mo_mailjet_confirm_email_message_content', function () {
+    return 'Thanks for subscribing to my email list. Please click the button below to complete your subscription.';
+});
+
+add_filter('mo_mailjet_confirm_email_button_text', function () {
+    return 'Confirm Subscription Now';
+});
