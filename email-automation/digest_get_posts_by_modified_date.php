01cef8e0c6261c78524ac001efd0583ec84da437
Collins Agbonghama <Collizo4sky@gmail.com>
2020-02-21 10:28:02 +0100

    Update digest_get_posts_by_modified_date.php

--- email-automation/digest_get_posts_by_modified_date.php
+++ email-automation/digest_get_posts_by_modified_date.php
@@ -3,7 +3,8 @@
  * By default, posts for email digest are fetched and ordered by their post published date.
  * If you want recently modified posts to be included in next batch of email digest, use this code.
  */
- add_filter('mo_post_digest_get_posts_args', function($parameters) {
-    $parameters['orderby'] = 'post_modified';
-    return $parameters;
- });
+add_filter('mo_post_digest_get_posts_args', function($parameters) {
+   $parameters['orderby'] = 'post_modified';
+   $parameters['date_query'][0]['column'] = 'post_modified';
+   return $parameters;
+});
