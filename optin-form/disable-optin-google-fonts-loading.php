28ceb98dcaa24329ba0696f15c2674a774ab535e
Collins Agbonghama <Collizo4sky@gmail.com>
2022-08-29 11:55:45 +0100

    Update disable-optin-google-fonts-loading.php

--- optin-form/disable-optin-google-fonts-loading.php
+++ optin-form/disable-optin-google-fonts-loading.php
@@ -1,3 +1,7 @@
 <?php
-/** Stop google font in any optin from loading */
+/** 
+ * Stop google font in any optin from loading
+ */
+add_filter('mo_optin_form_set_font_families_to_inherit', '__return_true');
+// for some odd reasons, when this isn't disabled, google fonts were being loaded by cache.
 add_filter('mailoptin_disable_optin_form_cache', '__return_true');
