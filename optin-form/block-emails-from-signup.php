8b46a8e176f6c6fa4a2fbdb76268497b15b5b306
Collins Agbonghama <Collizo4sky@gmail.com>
2023-03-21 14:31:27 +0100

    Create block-emails-from-signup.php

---
+++ optin-form/block-emails-from-signup.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Code snippets to block signups from specific email addresses.
+ */
+add_filter('mo_subscription_form_error', function ($response, ConversionDataBuilder $conversion_data) {
+
+    $blocked_email = [
+        'sample@gmail.com'
+    ];
+
+    if (in_array($conversion_data->email, $blocked_email)) {
+        return new \WP_Error('invalid_email', 'This email is blocked.');
+    }
+
+    return $response;
+
+}, 10, 2);
